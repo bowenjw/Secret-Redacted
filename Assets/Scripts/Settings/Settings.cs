e230f60afe1c1d17a6c03b4cd13b80e70e290607
Jacobsma <mitch5736@gmail.com>
2020-09-03 14:58:09 -0400

    Fixed resolution bugs
    There are still too many resolutions showing up in the resolution drop down.

--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
+using System.Linq;
 
 public class Settings : MonoBehaviour { 
 
@@ -17,12 +18,32 @@ public class Settings : MonoBehaviour {
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++){
             string option = resolutions[i].width + " x " + resolutions[i].height;
+            /*
+            if (options.Contains(option)){
+                continue;
+            }
+            */
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height ) {
+                currentResolutionIndex = i;
+            }
         }
 
+
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void setResolution (int resolutionIndex) {
+
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void setQuality( int qualityIndex ) {
