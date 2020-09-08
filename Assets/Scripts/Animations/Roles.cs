1c1d0dc5b9671d65a2ed96ed60a11868758cabc4
Jacobsma <mitch5736@gmail.com>
2020-09-08 16:43:00 -0400

    Implemented role generation
    Made role generation and implemented a function that reveals the roles of each player

---
+++ Assets/Scripts/Animations/Roles.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class Roles : MonoBehaviour {
+
+    public int players = 5; 
+    public string[] playerRoles;
+    public int[] amtFash = {2,2,3,3,4,4};
+    private Sprite[] liberalSprites;
+    private Sprite[] facistSprites;
+    private Sprite hitlerSprite;
+    private GameObject[] children;
+
+    void Start()
+    {
+        liberalSprites = Resources.LoadAll<Sprite>("Roles/Liberal");
+        facistSprites = Resources.LoadAll<Sprite>("Roles/Fascist");
+        hitlerSprite = Resources.Load<Sprite>("Roles/Hitler/Hitler");
+
+        //TODO: get # of players
+        playerRoles = new string[players];
+        var rand = new System.Random();
+
+        int maxFash = amtFash[players % 5];
+        int[] fashIndex = new int[maxFash];
+
+        for (int i = 0; i < maxFash; i++) {
+            fashIndex[i] = rand.Next(players); // rand goes between 0 - players 
+        }
+
+        children = new GameObject[players];
+
+        for (int i = 0; i < players; i++) {
+
+            children[i] = GameObject.Find("Player " + (i+1));
+
+            if ( fashIndex.Contains(i) ) {
+                // player is fash 
+                playerRoles[i] = "Fascist";
+            }
+            else {
+                playerRoles[i] = "Liberal";
+            }
+        }
+        playerRoles[fashIndex[0]] = "Hitler"; // first fash becomes hitler
+
+    }
+
+    public void assignRoles() {
+
+        int facistCnt = 0;
+
+        for (int i = 0; i < players; i++) {
+            SpriteRenderer x = children[i].GetComponent<SpriteRenderer>();
+            if (playerRoles[i] == "Liberal") {
+                x.sprite = liberalSprites[i]; 
+            }
+            else if (playerRoles[i] == "Fascist") {
+                x.sprite = facistSprites[facistCnt];
+                facistCnt++;
+            }
+            else {
+                // is hitler
+                x.sprite = hitlerSprite;
+            }
+            children[i].SetActive(false);
+        }
+        for (int i = 0; i < players; i++) {
+            children[i].SetActive(true);
+        }
+        
+    }
+
+    public void showRoles() {
+
+        for (int i = 0; i < players; i++) {
+           //sprite.sortingOrder++;
+
+        }
+    }
+        
+    public void hideRoles() {
+
+        for (int i = 0; i < players; i++) {
+           //sprite.sortingOrder--;
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
