f5c53fe0fd5634113a708f74e61c72a13a8b17d2
Kirtdeep Narain <naraink@sunypoly.edu>
2020-09-22 14:30:36 -0400

    Pathways Update
    -Adds Election Tracker
    -Adds Fascist Pathways for all players
    -Adds Debug.Log to annouce gamestate

--- Assets/Scripts/Animations/LiberalPathLogic.cs
+++ Assets/Scripts/Animations/LiberalPathLogic.cs
@@ -14,7 +14,7 @@ public class LiberalPathLogic : MonoBehaviour
     //overflowPreventation handles the main board, trackeroverflowPreventation handles the election tracker
     private SpriteRenderer sprite;
     int overflowPreventation = 0;
-    //int trackerOverflowPreventation = 0;
+    int trackerOverflowPreventation = 0;
 
     //Grabs current sorting order for liberal cards, each are done in a decsending order, 0, -1, -2 etc
     void Start()
@@ -50,6 +50,33 @@ public class LiberalPathLogic : MonoBehaviour
             DecrementLiberalPath();
     }
 
+    //Increments all trackers, allowing one tracker at a time to be seen, overflow prevents overpressing the button.
+    public void IncrementTracker()
+    {
+       if (trackerOverflowPreventation < 4) 
+       {
+           sprite.sortingOrder++;
+           trackerOverflowPreventation++;
+       }
+    }
+
+    //Decrements all trackers, getting rid of one tracker
+    public void DecrementTracker()
+    {
+       if (trackerOverflowPreventation > 0)
+       {
+           sprite.sortingOrder--;
+           trackerOverflowPreventation--;
+       }
+           
+    }
+
+    //Resets trackers
+    public void ResetTracker()
+    {
+        while (trackerOverflowPreventation != 0)
+            DecrementTracker();
+    }
 
     
 
